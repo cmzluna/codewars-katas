d3c6e8b24fb1f8f4662067f976bc00a79da9f59b
cmzluna <claudiomazzoli@gmail.com>
2021-07-08 11:10:20 -0300

    casino chips

--- testingPlace.js
+++ testingPlace.js
@@ -1,34 +1,29 @@
-function interleave(...args) {
-  console.log(args);
+const compare = (a, b) => b - a;
 
-  let res = [];
+function solve(arr) {
+  // a place to store the number of days
+  // initialized to zero
+  let numDays = 0;
 
-  while (!args.every((array) => array.length === 0)) {
-    args.forEach((el) => {
-      if (!el[0] && el[0] !== 0) {
-        res.push(null);
-      } else {
-        res.push(el[0]);
-      }
-      el.splice(0, 1);
-    });
+  arr.sort(compare);
+  // while there are at least 2 stacks with at least 1 chip
+  while (arr[0] > 0 && arr[1] > 0) {
+    // increment number of days
+    numDays++;
+    // decrement the 2 highest stacks
+    arr[0]--;
+    arr[1]--;
+    arr.sort(compare);
   }
-  return res;
+
+  return numDays;
 }
 
-console.log(interleave([1, 2, 3], [4, 5]), [1, 4, 2, 5, 3, null]);
-console.log(
-  interleave([1, 2, 3], [4, 5, 6], [7, 8, 9]),
-  [1, 4, 7, 2, 5, 8, 3, 6, 9]
-);
-console.log(interleave([1, 2, 3], ["c", "d", "e"]), [1, "c", 2, "d", 3, "e"]);
-console.log(interleave([]), []);
-
-console.log(interleave([4, 5], [1, 2, 3]))[
-  // problems in this one
-
-  (["X", "l", 2, "M"],
-  ["j", "7", "p", "X", "m", 0, "O", 8],
-  ["o", "L", 3, "g", "a", "a", 4, "i", "j", 0],
-  ["q", "v", "q", "h", 5, "v", "M"])
-];
+console.log(solve([1, 1, 1]), 1);
+console.log(solve([1, 2, 1]), 2);
+console.log(solve([4, 1, 1]), 2);
+console.log(solve([8, 2, 8]), 9);
+console.log(solve([8, 1, 4]), 5);
+console.log(solve([7, 4, 10]), 10);
+console.log(solve([12, 12, 12]), 18);
+console.log(solve([1, 23, 2]), 3);
